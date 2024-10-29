7d1284e3112b6a7a14cd78bea74a4063f740179b
MrGhostX <mrghostx94@gmail.com>
2024-10-29 17:58:46 +0100

    "Updated package-lock.json, package.json, and App.js files"

---
+++ src/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function body() {
+  return (
+    <div>
+      <div className="body">
+        <h1>hello world!</h1>
+        <p>xup.................🤣🤣🤣🤣🤣🤣🤣🤣🤣</p>
+      </div>
+    </div>
+  )
+}
+
+export default body
